ba1adfa5ecb278b7f1e309bb0b6c7d768a47a203
Olga <zhuravel.olga95@gmail.com>
2021-03-25 18:36:33 +0200

    webpack project

---
+++ webapp/src/index.js
@@ -0,0 +1,6 @@
+import "./jquery-3.6/jquery-3.6.min.js";
+import "./bootstrap-4.6.0/js/bootstrap.min.js";
+import "./bootstrap-4.6.0/css/bootstrap.min.css";
+import "./js/common.js";
+import "./assets/css/main.css";
+import "./assets/scss/main.scss";
