a4db4c5f8e37e15dc733a24325f880f71f0c89b0
Olga <zhuravel.olga95@gmail.com>
2021-03-26 17:32:18 +0200

    in process home page v1

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,6 +1,7 @@
-import "./jquery-3.6/jquery-3.6.min.js";
-import "./bootstrap-4.6.0/js/bootstrap.min.js";
-import "./bootstrap-4.6.0/css/bootstrap.min.css";
+// import "./js/jquery-3.6.min.js";
+import "./js/bootstrap.min.js";
 import "./js/common.js";
+
+import "./assets/css/bootstrap.min.css";
 import "./assets/css/main.css";
 import "./assets/scss/main.scss";
